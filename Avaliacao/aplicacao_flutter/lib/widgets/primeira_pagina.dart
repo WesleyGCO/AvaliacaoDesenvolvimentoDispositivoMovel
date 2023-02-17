e6bc25300e049fae8f90ccb451742e71b2bf6a73
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-02-17 20:42:28 -0300

    Atualizando mais configurações na primeira página

--- Avaliacao/aplicacao_flutter/lib/widgets/primeira_pagina.dart
+++ Avaliacao/aplicacao_flutter/lib/widgets/primeira_pagina.dart
@@ -7,6 +7,27 @@ class PrimeiraPagina extends StatelessWidget {
       appBar: AppBar(
         title: Text('Primeira página'),
       ),
+      body: Center(
+        //Deixar os componentes centralizados
+        child: Column(
+          //Todos os componentes definidos no array, ficará em coluna
+          children: [
+            // ignore: prefer_const_constructors
+            TextField(
+              cursorColor: Colors.green, // Define a cor do cursor
+              maxLength:
+                  250, // Define a quantidade de caracteres que podem ser digitados
+              decoration: InputDecoration(
+                  label: Text("Nome"), // Define o nome do campo a ser digitado
+                  hintText:
+                      'Informe seu nome'), // Define a dica do que é para colocar no campo
+            ),
+            ElevatedButton(
+                onPressed: () {}, //Faz nada, se declarar assim
+                child: Text("Ok"))
+          ],
+        ),
+      ),
     );
   }
 
