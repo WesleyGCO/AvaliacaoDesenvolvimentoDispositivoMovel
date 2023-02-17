45d33fc4d4139ad195a8dd9b438fceb1e07b704d
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-02-17 20:06:58 -0300

    Primeiro commit

---
+++ Avaliacao/aplicacao_flutter/lib/widgets/primeira_pagina.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PrimeiraPagina extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Primeira página'),
+      ),
+    );
+  }
+
+  // tentar rodar o aplicativo chamando este widget
+}
