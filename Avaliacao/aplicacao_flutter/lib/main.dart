eafe02c9a34ff694ffa4331f587eac237e0c3560
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-02-18 01:07:57 -0300

    Retirada de informações duplicadas e/ou redundantes

--- Avaliacao/aplicacao_flutter/lib/main.dart
+++ Avaliacao/aplicacao_flutter/lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:aplicacao_flutter/Configuracoes.dart';
-import 'package:aplicacao_flutter/widgets/primeira_pagina.dart';
+import 'package:aplicacao_flutter/configuracoes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
