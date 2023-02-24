c2a855f7b8af124b8bb8d17fa9f3ab96cd8028ea
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-02-24 19:59:57 -0300

    Fundamentos básicos da programação com Dart

--- Fundamentos/fundamentos_dart/bin/fundamentos_dart.dart
+++ Fundamentos/fundamentos_dart/bin/fundamentos_dart.dart
@@ -1,5 +1,46 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:fundamentos_dart/fundamentos_dart.dart' as fundamentos_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${fundamentos_dart.calculate()}!');
+  print('fundamentos de variáveis');
+  int idade = 19;
+  double peso = 80.5;
+  String nome = 'Wesley';
+  String sobrenome = 'Castilho';
+  bool eProgramador = true;
+
+  print(idade);
+  //Concatenação
+  print(nome + ' ' + sobrenome);
+
+  /*
+  INSERT INTO pessoa (nome, sobrenome) VALUES ('Wesley', 'Castilho')
+  */
+
+  // Mostrar um comando SQL no print
+  print("INSERT INTO pessoa (nome, sobrenome) VALUES ('" +
+      nome +
+      "','" +
+      sobrenome +
+      "')");
+  //para reconhecer que é uma variável dentro da string, colocar o símbolo $
+  print("INSERT INTO pessoa (nome, sobrenome) VALUES ('$nome', '$sobrenome')");
+
+  //pode ser usado assim também
+  String teste = 'teste ${5 > 0}';
+  print(teste); //Se 5 for maior que 0, retornará true
+
+  // $ (dólar) faz a leitura do campo ou instrução informada
+  String nomeCompleto = '$nome $sobrenome';
+
+  //uma forma de demonstrar informações a cada jeito
+  print('\nlinha1' + '\nlinha2' + '\nlinha3');
+
+  ///segunda forma de demonstrar informações a cada jeito
+  print('''
+    linha1
+    linha2
+    linha3
+  ''');
 }
