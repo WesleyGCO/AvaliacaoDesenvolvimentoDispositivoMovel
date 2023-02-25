7bda4cb71a785632edc5d6e98f6a86174054ce59
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-02-24 22:33:11 -0300

    Ajustes no exercício 1 - Calcular salario

--- Fundamentos/fundamentos_dart/bin/exercicio1_fixacao.dart
+++ Fundamentos/fundamentos_dart/bin/exercicio1_fixacao.dart
@@ -17,6 +17,7 @@ void main(List<String> arguments) {
   print(calcularSalario(valorSalario, valorDesconto));
 }
 
+// Função com retorno e com parametro
 String calcularSalario(double valorSalario, double valorDesconto) {
   if (valorDesconto > valorSalario) {
     return "O valor do desconto é maior do que o valor do salário!";
@@ -24,4 +25,28 @@ String calcularSalario(double valorSalario, double valorDesconto) {
     double saldoLiquidoSalario = valorSalario - valorDesconto;
     return "O salário líquido é $saldoLiquidoSalario reais";
   }
+}
+
+// Função sem retorno e sem parametro
+void calcularSalarioDois() {
+  double? valorSalario;
+  double? valorDesconto;
+  String resultado;
+
+  print("Calcule o seu salário");
+
+  print("Informe o valor do salário:");
+  valorSalario = double.parse(stdin.readLineSync()!);
+
+  print("Informe o valor do desconto:");
+  valorDesconto = double.parse(stdin.readLineSync()!);
+
+  if (valorDesconto > valorSalario) {
+    resultado = "O valor do desconto é maior do que o valor do salário!";
+  } else {
+    double saldoLiquidoSalario = valorSalario - valorDesconto;
+    resultado = "O salário líquido é $saldoLiquidoSalario reais";
+  }
+
+  print(resultado);
 }
