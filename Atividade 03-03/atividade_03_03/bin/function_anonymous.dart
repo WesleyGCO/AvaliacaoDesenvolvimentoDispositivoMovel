c04e12ac88177e70b96aa517f1ab38c6d38733e8
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-03-03 22:14:26 -0300

    Tópico: anonymous function ajustado

--- Atividade 03-03/atividade_03_03/bin/function_anonymous.dart
+++ Atividade 03-03/atividade_03_03/bin/function_anonymous.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:io';
 
 void main(List<String> arguments) {
   print("Tópico: funções anônimas!");
@@ -9,17 +9,58 @@ void main(List<String> arguments) {
   Essa função tem seu uso quando é algo simples e específico no local em que é necessária, 
   logo o sistema não precisa definir uma função separada com um nome exclusivo sendo que você
   resolver no mesmo ponto sem problemas.
+  É necessário ter uma função com o parâmetro do tipo função.
   */
 
-  //Exemplo
-  var listaNumeros = [23, 233, 45, 56, 77, 88];
+  print('''
+  Digite a operação deseja entre dois números:\n
 
-  // Pode ser usada com a função ForEach
-  listaNumeros.forEach((numero) {
-    print(numero * numero);
-  });
+  1) Aprovação pela média
+  2) Aprovação pela maior nota
+  3) Aprovação pela menor nota
+  ''');
 
-  // Pode ser usada com atribuição a uma variável
-  var soma = (int a, int b, int c, int d) => a + b + c + d;
-  print(soma(1, 5, 4, 45)); // Saída: 55
+  //Interações do usuário
+  int escolha = int.parse(stdin.readLineSync()!);
+  print('Informe a primeira nota:');
+  double n1 = double.parse(stdin.readLineSync()!);
+  print('Informe a segunda nota:');
+  double n2 = double.parse(stdin.readLineSync()!);
+  bool aprovado;
+
+  if (escolha == 1) {
+    aprovado = verificarAprovacao(n1, n2, calcularMedia);
+  } else if (escolha == 2) {
+    // Reescrevendo a função verificarMaiorNota em um função anônima - somente para comparação
+    aprovado = verificarAprovacao(n1, n2, (double nota1, double nota2) {
+      double maior = nota1;
+      if (nota2 > nota1) maior = nota2;
+      return maior;
+    });
+  } else {
+    aprovado = verificarAprovacao(n1, n2, calcularMenorNota);
+  }
+  print(aprovado ? 'aprovado' : 'reprovado');
+}
+
+bool verificarAprovacao(double nota1, double nota2, Function caclularNota) {
+  double nota = caclularNota(nota1, nota2);
+  return (nota >= 6);
+}
+
+double calcularMedia(double nota1, double nota2) {
+  double media = (nota1 + nota2) / 2;
+  return media;
+}
+
+double calcularMaiorNota(double nota1, double nota2) {
+  double maior = nota1;
+  if (nota2 > nota1) maior = nota2;
+  return maior;
+}
+
+double calcularMenorNota(double nota1, double nota2) {
+  double menor = nota1;
+  if (nota2 < nota1) menor = nota2;
+  return menor;
 }
