b535724ed3741454fbe539b9c070aef850b4575a
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-03-10 20:06:22 -0300

    Ajustando nome da classe Aluno para Aluno2

--- Aula POO/aula_poo/lib/classes/aluno2.dart
+++ Aula POO/aula_poo/lib/classes/aluno2.dart
@@ -1,9 +1,9 @@
-class Aluno {
+class Aluno2 {
   int RA;
   String nome;
   String CPF;
 
-  Aluno(this.RA, this.nome, this.CPF);
+  Aluno2(this.RA, this.nome, this.CPF);
 
-  Aluno.outroConstrutor(this.RA, this.nome, this.CPF);
+  Aluno2.outroConstrutor(this.RA, this.nome, this.CPF);
 }
