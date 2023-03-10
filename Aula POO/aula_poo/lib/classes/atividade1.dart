6acc4fc9d74363607b1399ccaa895e145a8198e6
Wesley Castilho <wesleyguilhermecastilho@gmail.com>
2023-03-10 20:44:22 -0300

    Criado arquivo para resolver a atividade 01

---
+++ Aula POO/aula_poo/lib/classes/atividade1.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments) {
+  /*
+  Atividade 01
+
+  - Criar professor com 2 atributos
+  - Alterar atributos do professor para não nulos
+  - Alterar para construtores nomeados
+  - Criar um objeto aluno no print
+  - Alterar professor para ter atributos nulos e não nulos
+  - Criar um objeto professor com função nomeada
+  - Criar um objeto professor com função anônima
+  - Criar um objeto professor com arrow function
+  - Criar um objeto professor com arrow function no print
+  */
+}
